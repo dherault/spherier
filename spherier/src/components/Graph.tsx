ab32f6e7388a3c9bfd710b08689b513ec101202d
David Hérault <dherault@gmail.com>
2024-05-17 05:08:31 +0200

    Add reactflow

--- spherier/src/components/Graph.tsx
+++ spherier/src/components/Graph.tsx
@@ -1,14 +1,40 @@
 'use client'
 
+import { useCallback, useMemo } from 'react'
+import ReactFlow, { type Connection, type Edge, addEdge, useEdgesState, useNodesState } from 'reactflow'
+
 import useBeings from '~/src/hooks/useBeings'
 
 function Graph() {
   const { beings } = useBeings()
 
+  const initialNodes = useMemo(() => beings.map(being => ({
+    id: being.id.toString(),
+    position: { x: being.x, y: being.y },
+    data: { label: being.name },
+  })), [beings])
+  const initialEdges: Edge[] = []
+
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes)
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges)
+
+  const onConnect = useCallback((params: Edge | Connection) => {
+    setEdges(eds => addEdge(params, eds))
+  }, [setEdges])
+
   return (
-    <pre>
-      {JSON.stringify(beings, null, 2)}
-    </pre>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        proOptions={{
+          hideAttribution: true,
+        }}
+      />
+    </div>
   )
 }
 
